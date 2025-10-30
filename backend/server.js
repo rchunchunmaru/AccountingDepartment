a976b3c8946cc346911bbb37d3396f23c000b57c
Adennovielle <bocoboadolfvincent@gmail.com>
2025-10-30 20:10:08 +0800

    node init

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const mongoose = require("mongoose");
-const path = require("path");
-require("dotenv").config();
-const uploadsDir = path.join(__dirname, "Uploads");
 app.use(cors());
 app.use(express.json());
+
 app.get("/", (req, res) => {
   res.json({ message: "hello world" });
 });
